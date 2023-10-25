bad179e54ce5ddcfc8aaf8a34e554c21f68079d7
wndgndi <wndgn456@gmail.com>
2023-10-25 13:34:11 +0900

    날씨 일기 프로젝트 생성

---
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -0,0 +1,15 @@
+package zerobase.weather;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+}
