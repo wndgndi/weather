b53afcc90fe3ab20336aed62af1bf55e380ca502
wndgndi <wndgn456@gmail.com>
2023-10-26 17:39:44 +0900

    파싱한 날씨 정보와 일기 db에 저장

--- src/main/java/zerobase/weather/domain/Diary.java
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -1,13 +1,26 @@
 package zerobase.weather.domain;
 
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Diary {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+    private String weather;
+    private String icon;
+    private double temperature;
+    private String text;
+    private LocalDate date;
 }
